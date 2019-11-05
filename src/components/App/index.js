aa811e7720a3c9670a6eb36e592e92a92b469078
William Muli <willi.wambu@gmail.com>
2019-11-06 00:19:24 +0300

    Added comicSelector

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,15 +1,21 @@
 import { connect } from 'react-redux';
 import actions from '../../store/comic/actions';
 import App from './App';
-import { hasMoreComics, selectComic } from '../../store/comic/selectors';
+import {
+  hasMoreComics,
+  selectComic,
+  comicsSelector
+} from '../../store/comic/selectors';
 
 const mapStateToProps = state => ({
   comic: state.comic,
   hasMoreComics: hasMoreComics(state),
-  selectComic: selectComic(state)
+  selectComic: selectComic(state),
+  comicsSelector: comicsSelector(state)
 });
 
 const mapDispatcher = dispatch => ({
+  getComic: comicId => dispatch(actions.getComic(comicId)),
   getComics: ({ page, limit }) => dispatch(actions.getComics({ page, limit }))
 });
 
