c351dd7fc19734f181c5e69f1d667e27198380fa
William Muli <willi.wambu@gmail.com>
2019-11-06 00:24:10 +0300

    Fetch data from the store or request from API

--- src/components/Comics/Comics.js
+++ src/components/Comics/Comics.js
@@ -1,38 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './Comics.scss';
 import ComicsList from '../ComicsList/ComicsList';
 
 class Comics extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	componentDidMount() {
-		this.getComics();
-	}
+  componentDidMount() {
+    const { comics, isFetching } = this.props.comic;
+    if (comics.length === 0 && !isFetching) {
+      // fetch from api
+      this.getComics();
+    }
+  }
 
-	getComics = () => {
-		const { comic } = this.props;
-		const { limit, page, isFetching } = comic;
+  getComics = () => {
+    const { limit, page } = this.props.comic;
 
-		if(!isFetching) {
-			this.props.getComics(page, limit)
-		}
-	};
+    this.props.getComics(page, limit);
+  };
 
-	render(props) {
-		const { hasMoreComics } = this.props;
-		const { comics, isFetching } = this.props.comic;
-		return (
-			<ComicsList
-				comics={comics}
-				hasMoreComics={hasMoreComics}
-				isFetching={isFetching}
-				getComics={this.getComics}
-			/>
-		);
-	}
+  render() {
+    const { hasMoreComics } = this.props;
+    const { comics, isFetching } = this.props.comic;
+    return (
+      <ComicsList
+        comics={comics}
+        hasMoreComics={hasMoreComics}
+        isFetching={isFetching}
+        getComics={this.getComics}
+      />
+    );
+  }
 }
 
 export default Comics;
