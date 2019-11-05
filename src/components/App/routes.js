09d4ecafa7af098998303c2e068ef5478252c677
William Muli <willi.wambu@gmail.com>
2019-11-06 00:20:28 +0300

    Passed routerProps to ComicWrapper component

--- src/components/App/routes.js
+++ src/components/App/routes.js
@@ -1,28 +1,34 @@
-import {Redirect, Route, Switch} from 'react-router';
+import { Redirect, Route, Switch } from 'react-router';
 import Comics from '../Comics/Comics';
 import Characters from '../Characters/Characters';
 import Games from '../Games/Games';
 import React from 'react';
 import ComicWrapper from '../ComicWrapper/ComicWrapper';
 
-export const Routes = (props) => (
-	<Switch>
-		<Route path="/characters">
-			<Characters />
-		</Route>
-		<Route path="/games">
-			<Games />
-		</Route>
-		<Route exact path="/comics">
-			<Redirect to="/" />
-		</Route>
-		<Route path="/comics/:comicId">
-			<ComicWrapper
-				selectComic={props.selectComic}
-			/>
-		</Route>
-		<Route path="/">
-			<Comics {...props} />
-		</Route>
-	</Switch>
+export const Routes = props => (
+  <Switch>
+    <Route path="/characters">
+      <Characters />
+    </Route>
+    <Route path="/games">
+      <Games />
+    </Route>
+    <Route exact path="/comics">
+      <Redirect to="/" />
+    </Route>
+    <Route
+      path="/comics/:comicId"
+      render={routerProps => (
+        <ComicWrapper
+		  {...routerProps}
+		  getComic={props.getComic}
+          isFetching={props.comic.isFetching}
+          comics={props.comic.comics}
+        />
+      )}
+    ></Route>
+    <Route path="/">
+      <Comics {...props} />
+    </Route>
+  </Switch>
 );
